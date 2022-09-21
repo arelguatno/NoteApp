7b9ef835c50cc8a6c45a8f648adfcdae8778e78c
Arel Guatno <arelguatno@yahoo.com>
2022-09-21 18:07:59 +0100

    more features

---
+++ app/src/main/java/com/example/noteapp/aye/fragments/list/ListFragment.kt
@@ -0,0 +1,23 @@
+package com.example.noteapp.aye.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.noteapp.aye.R
+import com.example.noteapp.aye.databinding.FragmentListBinding
+
+
+class ListFragment : Fragment() {
+    private lateinit var binding: FragmentListBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentListBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
