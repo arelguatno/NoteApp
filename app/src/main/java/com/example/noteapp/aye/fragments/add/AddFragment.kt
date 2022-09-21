510efd03e0fdfa7bb89b7065d42e278ea4d0b181
Arel Guatno <arelguatno@yahoo.com>
2022-09-21 19:22:03 +0100

    new data

--- app/src/main/java/com/example/noteapp/aye/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/noteapp/aye/fragments/add/AddFragment.kt
@@ -1,25 +1,78 @@
 package com.example.noteapp.aye.fragments.add
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Note
+import android.text.TextUtils
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.noteapp.aye.MainViewModel
 import com.example.noteapp.aye.R
+import com.example.noteapp.aye.data.Priority
 import com.example.noteapp.aye.databinding.FragmentAddBinding
 
 
 class AddFragment : Fragment() {
     private lateinit var binding: FragmentAddBinding
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentAddBinding.inflate(layoutInflater)
         setHasOptionsMenu(true)
         return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.add_fragment_menu,menu)
+        inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_add) {
+            insertDataToDb()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun insertDataToDb() {
+        val mTitle = binding.titleEt.text.toString()
+        val mPriority = binding.prioritiesSpinner.selectedItem.toString()
+        val mDescription = binding.descriptionEt.text.toString()
+
+        val validation = verifyDataUser(mTitle, mDescription)
+        if (validation) {
+
+            val newData = com.example.noteapp.aye.room_db.note_table.Note(
+                title = mTitle,
+                priority = parsePriority(mPriority),
+                description = mDescription
+            )
+            viewModel.insertRecord(newData)
+            findNavController().navigateUp()
+        }
+    }
+
+    private fun verifyDataUser(title: String, description: String): Boolean {
+        return if (TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
+            false
+        } else !(title.isEmpty() || description.isEmpty())
+    }
+
+    private fun parsePriority(priority: String): Priority {
+        return when (priority) {
+            "High Priority" -> {
+                Priority.High
+            }
+            "Medium Priority" -> {
+                Priority.High
+            }
+            "High Priority" -> {
+                Priority.High
+            }
+            else -> Priority.Low
+        }
     }
 }
