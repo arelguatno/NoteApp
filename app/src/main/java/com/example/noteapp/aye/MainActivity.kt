15348d83f72e73d5dffb3502b251a75727b4844a
Arel Guatno <arelguatno@yahoo.com>
2022-09-23 20:07:50 +0100

    navHost

--- app/src/main/java/com/example/noteapp/aye/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/aye/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.noteapp.aye
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.noteapp.aye.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,17 +14,21 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.navHostFragment)
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
